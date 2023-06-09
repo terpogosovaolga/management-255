3e9b8dfcdd2928bb21240a71abd1d02d0e2e6b82
terpogosovaolga <56823704+terpogosovaolga@users.noreply.github.com>
2023-06-09 15:20:15 +0300

    Add files via upload

--- src/pages/CreateProject.jsx
+++ src/pages/CreateProject.jsx
@@ -1,10 +1,22 @@
-import { useEffect, useState, useLocation } from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import LeftPanel from "../components/common/LeftPanel";
 import HomeHeader from "../components/home/HomeHeader";
 import LeftForm from "../components/createproject/LeftForm";
 import "../assets/css/form.css";
 import RightForm from "../components/createproject/RightForm";
+import projects from "../testdata/testdata.json";
 export default function CreateProject() {
+
+    const url = useLocation().pathname;
+    const id = url.split("/")[2];
+
+    console.log('id is ' + id);
+
+    const title = id ? "Редактирование проекта" : "Проекты студии"; 
+
+    console.log(title);
+    const project = id ? projects.find(p => p.id == id) : false;
 
     return (
         <div className="wrapper">
@@ -13,10 +25,10 @@ export default function CreateProject() {
 					<div className="container panel__wrapper">
                         <LeftPanel />
                         <div className="panel-content">
-                            <HomeHeader title={"Проекты студии"}/>
+                            <HomeHeader title={title}/>
                             <div className="editing__wrapper">
-                                <LeftForm />
-                                <RightForm />
+                                <LeftForm project={project}/>
+                                <RightForm project={project}/>
                             </div>
                         </div>
                     </div>
